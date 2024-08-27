18d36b2ec31c8cae5bfed43ebcf00d32cf19ab2f
satyam kumar <satyampandit021@gmail.com>
2024-08-27 12:46:40 +0530

    df

--- server.js
+++ server.js
@@ -11,18 +11,22 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB
- 
+
 mongoose
   .connect(
     "mongodb+srv://satyampandit021:20172522@rvbmhotelbooking.9hfzkrx.mongodb.net/itc?retryWrites=true&w=majority",
-    {},
+    {}
   )
   .then(() => console.log("Db Connected"))
   .catch((err) => console.log(err));
 
 const formSchema = new mongoose.Schema({
   name: String,
-  mobile: String,
+  mobile: {
+    type: String,
+    required: true,
+    unique: true
+  },
   email: String,
   address: String,
   area: String,
@@ -40,7 +44,11 @@ app.post("/api/submit", async (req, res) => {
   console.log(req.body);
   try {
     await formData.save();
-    await emailSender.welcomeEmail(req.body.email, req.body.mobile, req.body.name);
+    await emailSender.welcomeEmail(
+      req.body.email,
+      req.body.mobile,
+      req.body.name
+    );
     res.status(200).send("Form data saved successfully");
   } catch (error) {
     res.status(500).send("Failed to save form data");
@@ -80,12 +88,50 @@ app.post(`/api/leads`, async (req, res) => {
     return res.status(203).json({ message: "Something went wrong" });
   }
 });
+app.post(`/api/leads/:query`, async (req, res) => {
+  const { query } = req.params;
+  console.log("v");
+  try {
+    const leads = await Form.find({
+      $or: [
+        { name: { $regex: query, $options: "i" } },
+        { mobile: { $regex: query, $options: "i" } },
+        { email: { $regex: query, $options: "i" } },
+      ],
+    });
+    return res.status(200).json(leads);
+  } catch (error) {
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+});
+app.post(`/api/lead/delete/:id`, async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Form.findByIdAndDelete(id);
+    return res.status(200).json({ message: "Data Deleted Successfully!" });
+  } catch (error) {
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+app.post(`/api/lead/sendWelcome/:id`, async (req, res) => {
+  const { id } = req.params;
+  try {
+    const details = await Form.findById(id);
+    await emailSender.welcomeEmail(details.email, details.mobile, details.name);
+    return res
+      .status(200)
+      .json({ message: `Welcome Mail Sent Successfully! to ${details.name}` });
+  } catch (error) {
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+});
 
 app.get("/api", async (req, res) => {
   return res.send("Hello latest");
 });
 
-// Start the server 
+// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
